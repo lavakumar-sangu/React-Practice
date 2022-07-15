7954ba9ff96bddf206edbae05f4d855aba8d12d2
Lava <lsangu@keypoint-tech.com>
2022-07-15 11:12:40 +0530

    added new files for login and signup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,45 @@
 import React from 'react'
-
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Login from './components/loginpage/loginpage'
+import SignUp from './components/mainpage/mainpage'
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={'/sign-in'}>
+              <img className="navbar-img" src='https://st2.depositphotos.com/3867453/8451/v/950/depositphotos_84517826-stock-illustration-abstract-star-logo-icon-design.jpg'></img>
+            </Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-in'}>
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-up'}>
+                    Sign up
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route exact path="/" element={<Login />} />
+              <Route path="/sign-in" element={<Login />} />
+              <Route path="/sign-up" element={<SignUp />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
+  )
+}
+export default App
